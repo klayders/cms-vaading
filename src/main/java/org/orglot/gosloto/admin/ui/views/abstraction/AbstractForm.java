cec7bd24fdf2fc238896c33777ceb4bb82d550cc
Jura Kuznetsov <yk@finch-melrose.com>
2020-04-06 00:45:09 +0300

    refactoring

--- src/main/java/org/orglot/gosloto/admin/ui/views/abstraction/AbstractForm.java
+++ src/main/java/org/orglot/gosloto/admin/ui/views/abstraction/AbstractForm.java
@@ -14,6 +14,7 @@ import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 import java.util.Date;
@@ -62,91 +63,11 @@ public abstract class AbstractForm<T extends ManagedEntity> extends FormLayout {
     addListener(AbstractForm.SaveEvent.class, this::saveAchievementType);
     addListener(AbstractForm.DeleteEvent.class, this::deleteAchievementType);
     addListener(AbstractForm.CloseEvent.class, event -> this.closeEditor());
-
   }
 
   public void setEntity(T entity) {
     entityBinder.setBean(entity);
   }
-
-  private void collectFieldsForVaadinForm(Class<?> entity) {
-
-    final var className = entity.getSimpleName();
-
-    for (var field : entity.getDeclaredFields()) {
-
-      final var fieldName = field.getName();
-      final var i18nFieldName = applicationContext.getMessage(
-          ADMIN_I18N_PREFIX + className.toLowerCase() + "." + fieldName,
-          null,
-          fieldName,
-          Locale.getDefault()
-      );
-
-      AbstractField vaadinField = null;
-
-      if (field.getName().equals("createdById")) {
-        continue;
-      }
-
-      if (String.class.isAssignableFrom(field.getType())) {
-        vaadinField = new TextField(i18nFieldName);
-      } else if (NumbersHelper.BOOLEAN_VALUES.contains(field.getType())) {
-        vaadinField = new Checkbox(i18nFieldName);
-      } else if (NumbersHelper.FLOATING_NUMBERS.contains(field.getType())) {
-        vaadinField = new NumberField(i18nFieldName);
-      } else if (NumbersHelper.checkFieldType(field.getType(), Integer.class, int.class)) {
-        vaadinField = new IntegerField(i18nFieldName);
-      } else if (NumbersHelper.checkFieldType(field.getType(), Long.class, long.class)) {
-        vaadinField = new LongField(i18nFieldName);
-      } else if (NumbersHelper.checkFieldType(field.getType(), Byte.class, byte.class)) {
-        vaadinField = new ByteField(i18nFieldName);
-      } else if (NumbersHelper.checkFieldType(field.getType(), Short.class, short.class)) {
-        vaadinField = new ShortField(i18nFieldName);
-      } else if (Date.class.isAssignableFrom(field.getType())) {// Create a DateField with the default style
-        //        DateField date = new DateField();
-        //        vaadinField = new DateField(fieldName);
-      }
-
-      if (vaadinField != null) {
-        entityBinder.bind(vaadinField, fieldName);
-        vaadinComponentList.add(vaadinField);
-      }
-    }
-  }
-
-  //  public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
-  //                                                                ComponentEventListener<T> listener) {
-  ////    entityForm.addListener(AbstractForm.SaveEvent.class, entityForm::saveAchievementType);
-  ////    entityForm.addListener(AbstractForm.DeleteEvent.class, entityForm::deleteAchievementType);
-  ////    entityForm.addListener(AbstractForm.CloseEvent.class, event -> entityForm.closeEditor());
-  //
-  //    return getEventBus().addListener(eventType, listener);
-  //  }
-
-  private Component createButtonLayout() {
-    save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-    delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
-    close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-
-    save.addClickShortcut(Key.ENTER);
-    close.addClickShortcut(Key.ESCAPE);
-
-    save.addClickListener(click -> validateAndSave());
-    delete.addClickListener(click -> fireEvent(new DeleteEvent(this, entityBinder.getBean())));
-    close.addClickListener(click -> fireEvent(new CloseEvent(this)));
-
-    entityBinder.addStatusChangeListener(event -> save.setEnabled(entityBinder.isValid()));
-
-    return new HorizontalLayout(save, delete, close);
-  }
-
-  private void validateAndSave() {
-    if (entityBinder.isValid()) {
-      fireEvent(new SaveEvent(this, entityBinder.getBean()));
-    }
-  }
-
 
   public <T extends ManagedEntity> void deleteAchievementType(AbstractForm.DeleteEvent event) {
     //    achievementTypeService.delete(event.getEntity());
@@ -174,6 +95,80 @@ public abstract class AbstractForm<T extends ManagedEntity> extends FormLayout {
     setEntity(null);
     setVisible(false);
     removeClassName("editing");
+  }
+
+
+  private void collectFieldsForVaadinForm(Class<?> entity) {
+
+    final var className = entity.getSimpleName();
+
+    for (var field : entity.getDeclaredFields()) {
+      if (field.getName().equals("createdById")) {
+        continue;
+      }
+
+      final var fieldName = field.getName();
+      final var i18nFieldName = applicationContext.getMessage(
+          ADMIN_I18N_PREFIX + className.toLowerCase() + "." + fieldName,
+          null,
+          fieldName,
+          Locale.getDefault()
+      );
+
+      AbstractField vaadinField = resolveFieldAsVaadinAbstractField(field, i18nFieldName);
+
+      if (vaadinField != null) {
+        entityBinder.bind(vaadinField, fieldName);
+        vaadinComponentList.add(vaadinField);
+      }
+    }
+  }
+
+  private AbstractField resolveFieldAsVaadinAbstractField(Field field, String i18nFieldName) {
+    AbstractField vaadinField = null;
+    if (String.class.isAssignableFrom(field.getType())) {
+      vaadinField = new TextField(i18nFieldName);
+    } else if (NumbersHelper.BOOLEAN_VALUES.contains(field.getType())) {
+      vaadinField = new Checkbox(i18nFieldName);
+    } else if (NumbersHelper.FLOATING_NUMBERS.contains(field.getType())) {
+      vaadinField = new NumberField(i18nFieldName);
+    } else if (NumbersHelper.checkFieldType(field.getType(), Integer.class, int.class)) {
+      vaadinField = new IntegerField(i18nFieldName);
+    } else if (NumbersHelper.checkFieldType(field.getType(), Long.class, long.class)) {
+      vaadinField = new LongField(i18nFieldName);
+    } else if (NumbersHelper.checkFieldType(field.getType(), Byte.class, byte.class)) {
+      vaadinField = new ByteField(i18nFieldName);
+    } else if (NumbersHelper.checkFieldType(field.getType(), Short.class, short.class)) {
+      vaadinField = new ShortField(i18nFieldName);
+    } else if (Date.class.isAssignableFrom(field.getType())) {// Create a DateField with the default style
+      //        DateField date = new DateField();
+      //        vaadinField = new DateField(fieldName);
+    }
+    return vaadinField;
+  }
+
+
+  private Component createButtonLayout() {
+    save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+    close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+    save.addClickShortcut(Key.ENTER);
+    close.addClickShortcut(Key.ESCAPE);
+
+    save.addClickListener(click -> validateAndSave());
+    delete.addClickListener(click -> fireEvent(new DeleteEvent(this, entityBinder.getBean())));
+    close.addClickListener(click -> fireEvent(new CloseEvent(this)));
+
+    entityBinder.addStatusChangeListener(event -> save.setEnabled(entityBinder.isValid()));
+
+    return new HorizontalLayout(save, delete, close);
+  }
+
+  private void validateAndSave() {
+    if (entityBinder.isValid()) {
+      fireEvent(new SaveEvent(this, entityBinder.getBean()));
+    }
   }
 
 
